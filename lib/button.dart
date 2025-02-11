45ba9d55dd769fac9d3b82594c0c0a3ed4389737
Joan-Mena <toniomexico8@gmail.com>
2025-02-10 21:34:59 -0600

    final_commit

--- lib/button.dart
+++ lib/button.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 
 class Mybutton extends StatelessWidget {
   final child;
+  final function;
 
-  Mybutton({this.child});
+  Mybutton({this.child, this.function});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          padding: EdgeInsets.all(20),
-          color: Colors.grey[900],
-          child: Center(child: child),
+    return GestureDetector(
+      onTap: function,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            color: Colors.grey[900],
+            child: Center(child: child),
+          ),
         ),
       ),
     );
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:stacking_game_1/button.dart';
@@ -12,6 +14,97 @@ class homePage extends StatefulWidget {
 
 class _homePageState extends State<homePage> {
   int numberOfSquares = 160;
+  List<int> piece = [];
+  var direction = "left";
+  List<int> landed = [100000];
+  int level = 0;
+
+  void starGame() {
+    piece = [
+      numberOfSquares - 3 - level * 10,
+      numberOfSquares - 2 - level * 10,
+      numberOfSquares - 1 - level * 10
+    ];
+    Timer.periodic(Duration(milliseconds: 250), (timer) {
+      if (checkWinner()) {
+        _showDialog();
+        timer.cancel();
+      }
+      if (piece.first % 10 == 0) {
+        direction = "right";
+      } else if (piece.last % 10 == 9) {
+        direction = "left";
+      }
+      setState(() {
+        if (direction == "right") {
+          for (int i = 0; i < piece.length; i++) {
+            piece[i] += 1;
+          }
+        } else {
+          for (int i = 0; i < piece.length; i++) {
+            piece[i] -= 1;
+          }
+        }
+      });
+    });
+  }
+
+  bool checkWinner() {
+    if (landed.last < 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext Context) {
+          return AlertDialog(
+            title: Text("Winner!!!"),
+          );
+        });
+  }
+
+  void stack() {
+    setState(() {
+      level++;
+      for (int i = 0; i < piece.length; i++) {
+        landed.add(piece[i]);
+      }
+      if (level < 4) {
+        piece = [
+          numberOfSquares - 3 - level * 10,
+          numberOfSquares - 2 - level * 10,
+          numberOfSquares - 1 - level * 10
+        ];
+      } else if (level >= 4 && level < 8) {
+        piece = [
+          numberOfSquares - 2 - level * 10,
+          numberOfSquares - 1 - level * 10
+        ];
+      } else if (level >= 8) {
+        piece = [numberOfSquares - 1 - level * 10];
+      }
+      checkStack();
+    });
+  }
+
+  void checkStack() {
+    for (int i = 0; i < landed.length; i++) {
+      if (!landed.contains(landed[i] + 10) &&
+          (landed[i] + 10) < numberOfSquares - 1) {
+        landed.remove(landed[i]);
+      }
+    }
+    for (int i = 0; i < landed.length; i++) {
+      if (!landed.contains(landed[i] + 10) &&
+          (landed[i] + 10) < numberOfSquares - 1) {
+        landed.remove(landed[i]);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +119,19 @@ class _homePageState extends State<homePage> {
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 10),
                 itemBuilder: (BuildContext context, int index) {
-                  return MyPixel(
-                    color: Colors.black,
-                  );
+                  if (piece.contains(index)) {
+                    return MyPixel(
+                      color: Colors.white,
+                    );
+                  } else if (landed.contains(index)) {
+                    return MyPixel(
+                      color: Colors.white,
+                    );
+                  } else {
+                    return MyPixel(
+                      color: Colors.black,
+                    );
+                  }
                 }),
           ),
           Expanded(
@@ -37,12 +140,14 @@ class _homePageState extends State<homePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Mybutton(
+                    function: starGame,
                     child: Text(
                       "P L A Y",
                       style: TextStyle(color: Colors.white, fontSize: 30),
                     ),
                   ),
                   Mybutton(
+                    function: stack,
                     child: Text(
                       "S T O P",
                       style: TextStyle(color: Colors.white, fontSize: 30),
